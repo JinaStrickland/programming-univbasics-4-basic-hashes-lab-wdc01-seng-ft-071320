54d3100a2cb96e4b84b46b4bd24bd4abbdfb4fd6
Jina Strickland <jina30@gmail.com>
2020-06-30 17:34:56 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -3,17 +3,14 @@ def new_hash
 end
 
 def my_hash
-  {my_first_hash: "This is fun"
-  }
+  { my_first_hash: "This is fun" }
 end
 
 def pioneer
-  {:name => "Grace Hopper"
-  }
+  { :name => "Grace Hopper" }
 end
 
 
 def id_hash_generator(number)
-  {:id => number
-  }
+  { :id => number }
 end
