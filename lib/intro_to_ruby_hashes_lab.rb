3b2445166182081d7c4595047363f5db623cce35
Jina Strickland <jina30@gmail.com>
2020-06-30 17:36:50 +0000

    Done.

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,19 +1,16 @@
 def new_hash
-  Hash.new
+  {}
 end
 
 def my_hash
-  {my_first_hash: "This is fun"
-  }
+  { my_first_hash: "This is fun" }
 end
 
 def pioneer
-  {:name => "Grace Hopper"
-  }
+  { :name => "Grace Hopper" }
 end
 
 
 def id_hash_generator(number)
-  {:id => number
-  }
+  { :id => number }
 end
